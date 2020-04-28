bb47c76827a6ac390d6aec26aee1048de2253f0a
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-04-28 14:54:09 +0200

    added password to students table

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: { case_sensitive: false }
+  has_secure_password
 end
