91b67de19a19c4d1d982876fd5436e9590f0b0df
Grzegorz Dziedzic <grzegorzdziedzic@onet.pl>
2020-04-29 23:40:26 +0200

    added association and functionality to front end

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,6 @@ class Student < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :student_courses
+  has_many :courses, through: :student_courses
 end
